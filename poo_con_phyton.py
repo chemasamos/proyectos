e09c3f2a90cfb9f83fe22758a2616e06c93d9ff0
chemasamos <chemasassss@gmail.com>
2025-01-29 16:54:52 -0600

    Tarea final

--- poo_con_phyton.py
+++ poo_con_phyton.py
@@ -1,182 +1,124 @@
 class Character:
-    #Constructor de la clase(Atributos)
-    name = "Default"
-    strength = 0
-    intelligence = 0
-    defense = 0
-    life = 0
-    
-    #Constructor de la clase
     def __init__(self, name, strength, intelligence, defense, life):
         self.name = name
         self.strength = strength
         self.intelligence = intelligence
-        self.defense = defense 
+        self.defense = defense
         self.life = life
+        self.inventory = []
 
     def imprimir_atributos(self):
         print(self.name)
-        print("Strength: ",self.strength)
-        print("Inteligence: ",self.intelligence)
-        print("Defense: ",self.defense)
-        print("Life: ",self.life)
+        print("Strength:", self.strength)
+        print("Intelligence:", self.intelligence)
+        print("Defense:", self.defense)
+        print("Life:", self.life)
+        print("Inventory:", self.inventory)
 
     def level_up(self, strength, intelligence, defense):
-        self.strength = self.strength + strength
-        #self.strength += strength
-        self.intelligence = self.intelligence + intelligence
-        self.defense = self.defense + defense   
+        self.strength += strength
+        self.intelligence += intelligence
+        self.defense += defense   
 
     def is_alive(self):
         return self.life > 0
-    
+
     def die(self):
         self.life = 0
-        print("self.name has died")
-        #return self.life <= 0
+        print(f"{self.name} has died")
 
     def damage(self, enemy):
-        return self.strength - enemy.defense
-    
+        return max(0, self.strength - enemy.defense)
+
     def attack(self, enemy):
         damage = self.damage(enemy)
-        enemy.life = enemy.life - damage
-        #enemy.life -= damage
+        enemy.receive_damage(damage)
         print("------attack------")
         print(self.name, "has made", damage, "damage to", enemy.name)
-        print("life of" , enemy.name , "is", enemy.life)
-    
-class Figther(Character):
-     #Sobreescribir constructor
-    def __init__(self, name, strength, intelligence, defense, life, sword):
+        print("Life of", enemy.name, "is", enemy.life)
+
+    def receive_damage(self, damage):
+        self.life -= damage
+        if self.life <= 0:
+            self.die()
+
+    def use_potion(self, potion):
+        if potion in self.inventory:
+            if potion == "life":
+                self.life += 20
+            elif potion == "strength":
+                self.strength += self.strength * 0.5
+            elif potion == "intelligence":
+                self.intelligence += self.intelligence * 0.5
+            self.inventory.remove(potion)
+            print(f"{self.name} uso una {potion} potion!")
+        else:
+            print(f"{self.name} no uso {potion} potion!")
+
+class Fighter(Character):
+    def __init__(self, name, strength, intelligence, defense, life, sword, shield):
         super().__init__(name, strength, intelligence, defense, life)
         self.sword = sword
+        self.shield = shield
+        self.shield_life = defense * shield
 
-    #Sobreescribir impresión
     def imprimir_atributos(self):
         super().imprimir_atributos()
-        print("Sword: ", self.sword)
+        print("Sword:", self.sword)
+        print("Shield Life:", self.shield_life)
 
-    def elegir_arma(self):
-        opcion = int(input("Choose your weapon: \n 1.obsidian spear, daño 10 \n 2.chaya spear, daño 5 \n:"))
-        if opcion == 1:
-            self.sword = 10
-        elif opcion == 2:
-            self.sword = 5
-        else:
-            print("Invalid option")
-            self.elegir_arma()
-
-    #Sobreescribir calculo de daño
     def damage(self, enemy):
-        return self.strength + self.sword - enemy.defense
+        return max(0, self.strength + self.sword - enemy.defense)
+
+    def receive_damage(self, damage):
+        if self.shield_life > 0:
+            if damage < self.shield_life:
+                self.shield_life -= damage
+                print(f"{self.name} El escudo absorbio el daño total de: {self.shield_life}")
+            else:
+                remaining_damage = damage - self.shield_life
+                self.shield_life = 0
+                print(f"{self.name} el escudo se ha roto: {remaining_damage}")
+                super().receive_damage(remaining_damage)
+        else:
+            super().receive_damage(damage)
 
 class Wizard(Character):
-     #Sobreescribir constructor
     def __init__(self, name, strength, intelligence, defense, life, book):
         super().__init__(name, strength, intelligence, defense, life)
         self.book = book
 
-    #Sobreescribir impresión
     def imprimir_atributos(self):
         super().imprimir_atributos()
-        print("Book: ", self.book)
+        print("Book:", self.book)
 
-    def elegir_arma(self):
-        opcion = int(input("Choose your weapon: \n 1.Programming Spells, daño 10 \n 2.Chaya book, daño 5 \n:"))
-        if opcion == 1:
-            self.book = 10
-        elif opcion == 2:
-            self.book = 5
-        else:
-            print("Invalid option")
-            self.elegir_arma()
-
-    #Sobreescribir calculo de daño
     def damage(self, enemy):
-        return self.intelligence*self.book - enemy.defense
+        return max(0, self.intelligence * self.book - enemy.defense)
 
-michael_jackson = Character("Michael jackson", 20, 15, 10, 100)
-tlatoani = Figther("Apocalipto", 50,70,30,100,5)
-merlin = Wizard("Merlin", 20, 15, 10, 100, 5)
+goku = Fighter("Goku", 50, 20, 10, 100, 5, 2)
+vegeta = Fighter("Vegeta", 55, 25, 15, 100, 6, 3)
+piccolo = Wizard("Piccolo", 20, 50, 10, 100, 5)
 
-# Imprimikr atributos antes de la pelea
-tlatoani.imprimir_atributos()
+goku.inventory.append("life")
+vegeta.inventory.append("strength")
+piccolo.inventory.append("intelligence")
+
+goku.imprimir_atributos()
 print("------------")
-merlin.imprimir_atributos()
+vegeta.imprimir_atributos()
 print("------------")
-michael_jackson.imprimir_atributos()
+piccolo.imprimir_atributos()
 
-#Ataque masivos
-michael_jackson.attack(tlatoani,merlin)
-tlatoani.attack(michael_jackson,merlin)
-merlin.attack(michael_jackson,tlatoani) 
+goku.attack(vegeta)
+vegeta.attack(goku)
+piccolo.attack(goku)
 
-tlatoani.imprimir_atributos()
+goku.use_potion("life")
+vegeta.use_potion("strength")
+piccolo.use_potion("intelligence")
+
+goku.imprimir_atributos()
 print("------------")
-merlin.imprimir_atributos()
+vegeta.imprimir_atributos()
 print("------------")
-michael_jackson.imprimir_atributos()
-
-def personaje_con_mas_vida(personajes):
-    personaje = max(personajes, key=lambda p: p.vida)
-    print(f"El personaje con más vida es {personaje.nombre} con {personaje.vida} puntos de vida.")
-personaje_con_mas_vida(personajes)
-
-def suma_total_inteligencia(personajes):
-    inteligencia_total = sum(p.inteligencia for p in personajes)
-    print(f"La inteligencia total de los personajes es: {inteligencia_total}")
-
-suma_total_inteligencia(personajes)
-
-def personajes_con_vida_mayor(personajes, valor_vida):
-    filtrados = [p for p in personajes if p.vida > valor_vida]
-    if filtrados:
-        print(f"Los personajes con vida mayor a {valor_vida} son:")
-        for p in filtrados:
-            print(f"- {p.nombre}: {p.vida} puntos de vida")
-    else:
-        print(f"No hay personajes con vida mayor a {valor_vida}.")
-
-personajes_con_vida_mayor(personajes, valor_vida_usuario)
-
-
-# tlatoani.elegir_arma()
-# merlin.elegir_arma()
-# print(tlatoani.sword)
-
-
-# #Variable del constructor vacio que almacena la clase
-# print("------------Character------------")
-# my_character = Character("Dante", 100, 3, 70, 100)
-# my_character.imprimir_atributos()
-
-# my_enemy = Character("Vergil", 70, 30, 70, 100)
-# # print(my_character.damage(my_enemy))
-
-# my_character.attack(my_enemy)
-# print("------------Enemy after attack------------")
-# my_enemy.imprimir_atributos()
-
-
-    
-
-
-#print(my_character.is_alive())
-
-# print("------------Level up------------")
-# my_character.level_up(10, 1, 5)
-# my_character.imprimir_atributos()
-
-# my_character.name = "Pompompurin"
-# my_character.strength = 10
-# my_character.intelligence = 10
-# my_character.defense = 10
-# my_character.life = 100
-
-#Imprimir la variable con su respectivo atributo
-# print("The name of my character is:" ,my_character.name)
-# print("The strength of my character is:" ,my_character.strength)
-# print("The intelligence of my character is:" ,my_character.intelligence)   
-# print("The defense of my character is:" ,my_character.defense)
-# print("The life of my character is:" ,my_character.life)    
+piccolo.imprimir_atributos()
